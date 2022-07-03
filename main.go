547859835b5c706ca4a531b1c288defc0baed315
KaranP3 <karanpillai93@gmail.com>
2022-07-04 05:00:02 +0530

    Add flag and logging

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"io/fs"
 	"log"
 	"net/http"
@@ -11,11 +12,18 @@ import (
 var embeddedFiles embed.FS
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	fsys, err := fs.Sub(embeddedFiles, "frontend/public")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.Handle("/", http.FileServer(http.FS(fsys)))
-	http.ListenAndServe(":4000", nil)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.FS(fsys)))
+
+	log.Printf("Starting server on %s", *addr)
+	err = http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
